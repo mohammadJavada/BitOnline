aea484fd69b7013b4e4e7581bccc3d0c01b9fb9f
mohammadjavad <mohammadjavada78@gmail.com>
2022-07-07 14:00:01 +0430

    change homepage data

---
+++ bitonline/src/components/page/home/components/fetchData/index.ts
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function useFetch(
+  url: string = "",
+  dataList: any[] = [],
+  page: number = 0,
+  search: string = "",
+  sort: number = 2,
+  isFetch: boolean = false
+) {
+  console.log(search);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(dataList);
+  useEffect(() => {
+    (async function () {
+      if (isFetch) {
+        try {
+          setLoading(true);
+          const response = await axios.get(
+            `${url}?page=${page}&sort=${sort}&q=${search}`
+          );
+          const { data }: any = response;
+          console.log(data);
+          const list = data?.result?.items;
+          setData((post: any) => [...post, ...list]);
+        } catch (err: any) {
+          setError(err);
+        } finally {
+          setLoading(false);
+        }
+      }
+    })();
+  }, [page, sort, search, url, isFetch]);
+
+  return { data, error, loading };
+}
--- bitonline/src/components/page/home/index.tsx
+++ bitonline/src/components/page/home/index.tsx
@@ -1,38 +1,100 @@
+// import axios from "axios";
+// import type { NextPage } from "next";
+// import Head from "next/head";
+// import { useContext, useEffect, useState } from "react";
+// import InfiniteScroll from "react-infinite-scroll-component";
+// import { HomeContext } from "../../../context";
+// import SortData from "./components/SortData";
+// import HomeTable from "./components/Table";
+// import CS from "./index.module.scss";
+
+// const HomePage: NextPage = (currency) => {
+//   const { result }: any = currency;
+//   const { items, meta } = result;
+//   const { paginateHelper, prices } = meta;
+//   const { lastPage, total } = paginateHelper;
+//   const [list, setList] = useState([...items]);
+//   const [pages, setPages] = useState(2);
+//   const [news, setNews] = useState([]);
+
+//   const fetchData = async () => {
+//     setPages(pages + 1);
+//     console.log(pages);
+//     const { data } = await axios.get(
+//       `https://api.bitbarg.me/api/v1/currencies?page=${pages}`
+//     );
+//     const list = data.result.items;
+//     setNews(list);
+
+//     return list;
+//   };
+//   const data = useContext(HomeContext);
+//   const { currencyList, search } = data;
+
+//   useEffect(() => {
+//     setList([...list, ...news]);
+//   }, [news]);
+
+//   return (
+//     <div className={CS.container}>
+//       <Head>
+//         <title>قیمت لحظه ای | بیت برگ</title>
+//       </Head>
+//       <div>
+//         {/* @ts-ignore */}
+//         <SortData total={total} />
+//         <InfiniteScroll
+//           dataLength={list.length}
+//           next={fetchData}
+//           hasMore={pages <= lastPage}
+//           loader={<h4></h4>}
+//         >
+//           <HomeTable
+//             data={search ? currencyList?.result?.items : list}
+//             meta={prices}
+//           />
+//         </InfiniteScroll>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default HomePage;
+
 import axios from "axios";
 import type { NextPage } from "next";
 import Head from "next/head";
 import { useContext, useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { HomeContext } from "../../../context";
+import useFetch from "./components/fetchData";
 import SortData from "./components/SortData";
 import HomeTable from "./components/Table";
 import CS from "./index.module.scss";
 
 const HomePage: NextPage = (currency) => {
+  const data = useContext(HomeContext);
+  const { currencyList, search, sort } = data;
   const { result }: any = currency;
   const { items, meta } = result;
   const { paginateHelper, prices } = meta;
   const { lastPage, total } = paginateHelper;
-  const [list, setList] = useState([...items]);
-  const [pages, setPages] = useState(2);
-  const [news, setNews] = useState([]);
+  const [pages, setPages] = useState(search ? 0 : 1);
+  const [isFetch, setIsFetch] = useState(false);
 
-  const fetchData = async () => {
-    setPages(pages + 1);
-    const { data } = await axios.get(
-      `https://api.bitbarg.me/api/v1/currencies?page=${pages}`
-    );
-    const list = data.result.items;
-    setNews(list);
-
-    return list;
+  const fetchMore = () => {
+    setPages((page) => page + 1);
+    setIsFetch(true);
   };
-  const data = useContext(HomeContext);
-  const { currencyList, search } = data;
 
-  useEffect(() => {
-    setList([...list, ...news]);
-  }, [news]);
+  const { data: test } = useFetch(
+    `https://api.bitbarg.me/api/v1/currencies`,
+    isFetch ? [] : items,
+    pages,
+    search,
+    sort,
+    isFetch
+  );
 
   return (
     <div className={CS.container}>
@@ -43,15 +105,12 @@ const HomePage: NextPage = (currency) => {
         {/* @ts-ignore */}
         <SortData total={total} />
         <InfiniteScroll
-          dataLength={list.length}
-          next={fetchData}
+          dataLength={test.length}
+          next={fetchMore}
           hasMore={pages <= lastPage}
           loader={<h4></h4>}
         >
-          <HomeTable
-            data={search ? currencyList?.result?.items : list}
-            meta={prices}
-          />
+          <HomeTable data={test} meta={prices} />
         </InfiniteScroll>
       </div>
     </div>
