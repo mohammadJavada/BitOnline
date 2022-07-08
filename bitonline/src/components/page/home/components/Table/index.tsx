7b9ca5117c0abc3510bbcbd4fc547d1867eff40d
mohammadjavad <mohammadjavada78@gmail.com>
2022-07-08 21:38:48 +0430

    skip

--- bitonline/src/components/page/home/components/Table/index.tsx
+++ bitonline/src/components/page/home/components/Table/index.tsx
@@ -61,110 +61,104 @@ export default function HomeTable({ data = [], meta, isLoading }: any) {
         </TableHead>
 
         <TableBody>
-          {data.length
-            ? data.map((row: any, i: any) => (
-                <TableRow
-                  key={i}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+          {data.length ? (
+            data.map((row: any, i: any) => (
+              <TableRow
+                key={i}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="right">
+                  {
+                    <Button variant="text">
+                      <Image src={starImg} alt="*" width={20} height={20} />
+                    </Button>
+                  }
+                </TableCell>
+                <TableCell
+                  align="right"
+                  width="15%"
+                  className={
+                    row.percent === 0
+                      ? CS.percent
+                      : row.percent > 0
+                      ? CS.positive
+                      : CS.negative
+                  }
                 >
-                  <TableCell align="right">
-                    {
-                      <Button variant="text">
-                        <Image src={starImg} alt="*" width={20} height={20} />
-                      </Button>
-                    }
-                  </TableCell>
-                  <TableCell
-                    align="right"
-                    width="15%"
-                    className={
-                      row.percent === 0
-                        ? CS.percent
-                        : row.percent > 0
-                        ? CS.positive
-                        : CS.negative
-                    }
-                  >
-                    <div className={CS.percentdir}>{row.percent}%</div>
-                  </TableCell>
-                  <TableCell align="right" width="15%">
-                    {row?.chart?.length ? (
-                      <HistoryLineChart
-                        history={row.chart}
-                        status={row.percent > 0 ? "Y" : "N"}
-                        width={"145px"}
-                        height={"40px"}
+                  <div className={CS.percentdir}>{row.percent}%</div>
+                </TableCell>
+                <TableCell align="right" width="15%">
+                  {row?.chart?.length ? (
+                    <HistoryLineChart
+                      history={row.chart}
+                      status={row.percent > 0 ? "Y" : "N"}
+                      width={"145px"}
+                      height={"40px"}
+                    />
+                  ) : (
+                    ""
+                  )}
+                </TableCell>
+                <TableCell align="left" width="15%">
+                  <div className={CS.price}>
+                    <span className={CS.tableHeader}>
+                      {Price.seperate(checkPrice(row?.price, "sell"))}
+                    </span>
+                    <span>
+                      {type === "تومان" ? (
+                        " تومان"
+                      ) : (
+                        <Image src={tetherImg} alt="*" width={20} height={20} />
+                      )}
+                    </span>
+                  </div>
+                </TableCell>
+                <TableCell align="left" width="15%">
+                  <div className={CS.price}>
+                    <span className={CS.tableHeader}>
+                      {Price.seperate(checkPrice(row?.price, "buy"))}
+                    </span>
+                    <span>
+                      {type === "تومان" ? (
+                        " تومان"
+                      ) : (
+                        <Image src={tetherImg} alt="*" width={20} height={20} />
+                      )}
+                    </span>
+                  </div>
+                </TableCell>
+                <TableCell align="left" width="15%">
+                  <div className={CS.imageField}>
+                    <div className={CS.currencyName}>
+                      <div className={CS.currencyEnName}>{row?.enName}</div>
+                      <div className={CS.currencySymbol}>
+                        <div className={CS.numberPrefix}>{i + 1}</div>
+                        <div>{row?.coin}</div>
+                      </div>
+                    </div>
+                    <div>
+                      <Image
+                        priority
+                        src={row.icon}
+                        loader={() => row.icon}
+                        unoptimized={true}
+                        width={36}
+                        height={36}
+                        alt={"*"}
                       />
-                    ) : (
-                      ""
-                    )}
-                  </TableCell>
-                  <TableCell align="left" width="15%">
-                    <div className={CS.price}>
-                      <span className={CS.tableHeader}>
-                        {Price.seperate(checkPrice(row?.price, "sell"))}
-                      </span>
-                      <span>
-                        {type === "تومان" ? (
-                          " تومان"
-                        ) : (
-                          <Image
-                            src={tetherImg}
-                            alt="*"
-                            width={20}
-                            height={20}
-                          />
-                        )}
-                      </span>
                     </div>
-                  </TableCell>
-                  <TableCell align="left" width="15%">
-                    <div className={CS.price}>
-                      <span className={CS.tableHeader}>
-                        {Price.seperate(checkPrice(row?.price, "buy"))}
-                      </span>
-                      <span>
-                        {type === "تومان" ? (
-                          " تومان"
-                        ) : (
-                          <Image
-                            src={tetherImg}
-                            alt="*"
-                            width={20}
-                            height={20}
-                          />
-                        )}
-                      </span>
-                    </div>
-                  </TableCell>
-                  <TableCell align="left" width="15%">
-                    <div className={CS.imageField}>
-                      <div className={CS.currencyName}>
-                        <div className={CS.currencyEnName}>{row?.enName}</div>
-                        <div className={CS.currencySymbol}>
-                          <div className={CS.numberPrefix}>{i + 1}</div>
-                          <div>{row?.coin}</div>
-                        </div>
-                      </div>
-                      <div>
-                        <Image
-                          priority
-                          src={row.icon}
-                          loader={() => row.icon}
-                          unoptimized={true}
-                          width={36}
-                          height={36}
-                          alt={"*"}
-                        />
-                      </div>
-                    </div>
-                  </TableCell>
-                </TableRow>
-              ))
-            : ""}
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell component="th" scope="row"></TableCell>
+            </TableRow>
+          )}
         </TableBody>
       </Table>
-      {!data?.lenght && isLoading !== true ? (
+      {!data?.length && isLoading !== true ? (
         <div className={CS.emptyImage}>
           <Image src={emptyImg} alt="*" width="100" />
         </div>
